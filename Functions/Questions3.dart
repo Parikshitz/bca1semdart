87b44e0434f1bf9c1aff58528ce97692b3d17dc2
Parikshitz <parikshitz237@gmail.com>
2024-03-07 12:55:03 +0545

    main

--- Functions/Questions3.dart
+++ Functions/Questions3.dart
@@ -1,30 +1,32 @@
 import 'dart:io';
 
 void main() {
-  for (int x = 1; x <=5; x++) {
-    for (int i = 1; i <=x; i++) {
+  for (int x = 1; x <= 5; x++) {
+    for (int i = 1; i <= x; i++) {
       stdout.write(" * ");
     }
-      stdout.write("\n");
+    stdout.write("\n");
   }
+    print(" *  * moon *  *");
 
   for (int x = 5; x >= 1; x--) {
-    for (int i = 1; i <=x; i++) {
+    for (int i = 1; i <= x; i++) {
       stdout.write(" * ");
     }
-      stdout.write("\n");
-  }
-  for (int x = 1; x <=5; x++) {
-    for (int i = 1; i <=x; i++) {
-      stdout.write(" * ");
-    }
-      stdout.write("\n");
+    stdout.write("\n");
   }
 
-  for (int x = 5; x >= 1; x--) {
-    for (int i = 1; i <=x; i++) {
+  for (int x = 1; x <= 5; x++) {
+    for (int i = 1; i <= x; i++) {
       stdout.write(" * ");
     }
-      stdout.write("\n");
+    stdout.write("\n");
+  }
+        print(" *  * star *  *");
+  for (int x = 5; x >= 1; x--) {
+    for (int i = 1; i <= x; i++) {
+      stdout.write(" * ");
+    }
+    stdout.write("\n");
   }
 }
