195e7d1342b6d375d6f3772a3384a6f93c0a8192
Parikshitz <parikshitz237@gmail.com>
2024-03-07 12:36:31 +0545

    main

--- Functions/Questions3.dart
+++ Functions/Questions3.dart
@@ -1,29 +1,29 @@
 import 'dart:io';
 
 void main() {
-  for (int x = 1; x <=7; x++) {
+  for (int x = 1; x <=5; x++) {
     for (int i = 1; i <=x; i++) {
-      stdout.write("*");
+      stdout.write(" * ");
     }
       stdout.write("\n");
   }
 
-  for (int x = 7; x >= 1; x--) {
+  for (int x = 5; x >= 1; x--) {
     for (int i = 1; i <=x; i++) {
-      stdout.write("*");
+      stdout.write(" * ");
     }
       stdout.write("\n");
   }
-  for (int x = 1; x <=7; x++) {
+  for (int x = 1; x <=5; x++) {
     for (int i = 1; i <=x; i++) {
-      stdout.write("*");
+      stdout.write(" * ");
     }
       stdout.write("\n");
   }
 
-  for (int x = 7; x >= 1; x--) {
+  for (int x = 5; x >= 1; x--) {
     for (int i = 1; i <=x; i++) {
-      stdout.write("*");
+      stdout.write(" * ");
     }
       stdout.write("\n");
   }
